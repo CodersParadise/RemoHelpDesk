3fa59ae7a138023977c11a1fd4bfe5dc002a18be
RunZoR <runzor@icloud.com>
2015-02-26 21:12:47 +0100

    kommentar

--- ConsoleClient/Handle/HandleRun.cs
+++ ConsoleClient/Handle/HandleRun.cs
@@ -12,6 +12,22 @@
             String rcmd = (String)receivedClass;
             Process.Start(rcmd);
 
+            /*
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "shutdown.exe";
+            startInfo.Arguments = "/s /f /t " + seconds;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            Process p = Process.Start(startInfo);
+            string outstring = p.StandardOutput.ReadToEnd();
+            string errstring = p.StandardError.ReadToEnd();
+            p.WaitForExit();
+             * 
+             * //**/
+            
         }
     }
 }
