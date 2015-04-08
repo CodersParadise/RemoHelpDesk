d74c165f54b46b90255e3d63b125649af7afae90
sebastian.heinz.gt <sebastian.heinz.gt@googlemail.com>
2015-04-08 10:27:58 +0200

    added missing HandleChat

---
+++ GuiServer/Server/Handle/HandleChat.cs
@@ -0,0 +1,14 @@
+﻿namespace GuiServer.Server.Handle
+{
+    using GuiServer.Handle;
+    using GuiServer.View.ViewModel;
+
+    public class HandleChat : IHandlePacket
+    {
+        public void Handle(object receivedClass, ClientViewModel clientViewModel)
+        {
+            string message = receivedClass as string;
+            clientViewModel.UpdateChat(message);
+        }
+    }
+}
